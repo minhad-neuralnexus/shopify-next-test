1c9c91e62a8e5498c0dd90332faab326448232d9
Minhad Rizwan <minhadrizwan@Minhads-MacBook-Air.local>
2025-05-02 15:22:45 +0530

    added async script

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <Head>
         <meta name="shopify-api-key" content={process.env.SHOPIFY_API_KEY} />
-        <script src="https://cdn.shopify.com/shopifycloud/app-bridge.js"></script>
+        <script
+          async
+          src="https://cdn.shopify.com/shopifycloud/app-bridge.js"
+        ></script>
       </Head>
       <body>{children}</body>
     </html>
